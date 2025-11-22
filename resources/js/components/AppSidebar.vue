264e37e03d864da8610002d653c3a5c437fb0bd9
Jean Sacren <sakiwit@gmail.com>
2025-11-22 13:10:21 -0700

    Update sidebar links
    Use the Github Repo of this app to replace the vue-starter-kit.  Remove
    the Documentation link as it is not applicable.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -33,13 +33,8 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/sacren/Molecule',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
     },
 ];
 </script>
